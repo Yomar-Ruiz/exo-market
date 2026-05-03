9b62c1b930b52a2f69d8adeece3f3b0478ac106c
Yomar G Ruiz <yomargabrielruiz@gmail.com>
2026-05-02 21:02:10 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import TarjetaProducto from "@/components/TarjetaProducto"; // Importamos el componente
+import TarjetaProducto from "@/components/TarjetaProducto";
 
 interface Producto {
   id: number;
@@ -9,9 +9,8 @@ interface Producto {
 }
 
 export default async function Home() {
-try {
+  try {
     const respuesta = await fetch("https://fakestoreapi.com/products?limit=9", {
-      // Esto le dice a Next.js que si falla, intente de nuevo o maneje el error
       cache: 'no-store' 
     });
 
@@ -19,18 +18,26 @@ try {
       throw new Error("Error al conectar con la tienda");
     }
 
-  return (
-    <main className="p-10 bg-gray-50 min-h-screen">
-      <h1 className="text-4xl font-extrabold mb-10 text-center text-gray-800">
-        Eco-Market <span className="text-green-600">Digital</span> 🌿
-      </h1>
+    const productos: Producto[] = await respuesta.json();
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {productos.map((item) => (
-          // Usamos el componente y le pasamos el producto como "prop"
-          <TarjetaProducto key={item.id} producto={item} />
-        ))}
+    return (
+      <main className="p-10 bg-gray-50 min-h-screen">
+        <h1 className="text-4xl font-extrabold mb-10 text-center text-gray-800">
+          Eco-Market <span className="text-green-600">Digital</span> 🌿
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {productos.map((item) => (
+            <TarjetaProducto key={item.id} producto={item} />
+          ))}
+        </div>
+      </main>
+    );
+  } catch (error) {
+    return (
+      <div className="p-10 text-center text-red-500">
+        Error al cargar los productos.
       </div>
-    </main>
-  );
-}}
+    );
+  }
+}
